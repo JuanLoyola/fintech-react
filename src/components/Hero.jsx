ca6ccdaf0f920732591f0f37dd1c2ce08e1a5bc2
Juan Loyola <juanjoloyola@hotmail.com>
2023-02-17 15:53:37 -0300

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,11 +3,34 @@ import React, { Component } from 'react'
 export default class Hero extends Component {
     render() {
         return (
-            <div className='w-full h-screen flex justify-center items-center'>
-                <div className='w-full lg:max-w-3xl flex justify-between items-center px-12 text-sm'>
-                    <span className="flex flex-col">
-                        <h1>Compare and apply free</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, sapiente? Atque accusantium praesentium doloribus maxime! Illo, mollitia.</p>
+            <div className='w-full h-[100vh] md:h-[90vh] flex justify-center items-center'>
+                <div className='w-full lg:max-w-5xl flex flex-col-reverse md:flex-row gap-x-7 justify-center items-center text-sm'>
+                    <span className="w-4/5 md:w-3/6 h-[20rem] flex flex-col gap-y-5 justify-center items-center md:items-start">
+                        <h1 className='text-lg md:text-6xl font-bold mb-3'>Compare and apply free</h1>
+                        <p className='text-base font-medium md:text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, sapiente? Atque accusantium praesentium doloribus maxime! Illo, mollitia.</p>
+                        <div className='w-full flex justify-center md:justify-start items-center gap-x-5'>
+                            <a href="" target="_blank" rel="noopener noreferrer">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 text-black ">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 3.03v.568c0 .334.148.65.405.864l1.068.89c.442.369.535 1.01.216 1.49l-.51.766a2.25 2.25 0 01-1.161.886l-.143.048a1.107 1.107 0 00-.57 1.664c.369.555.169 1.307-.427 1.605L9 13.125l.423 1.059a.956.956 0 01-1.652.928l-.679-.906a1.125 1.125 0 00-1.906.172L4.5 15.75l-.612.153M12.75 3.031a9 9 0 00-8.862 12.872M12.75 3.031a9 9 0 016.69 14.036m0 0l-.177-.529A2.25 2.25 0 0017.128 15H16.5l-.324-.324a1.453 1.453 0 00-2.328.377l-.036.073a1.586 1.586 0 01-.982.816l-.99.282c-.55.157-.894.702-.8 1.267l.073.438c.08.474.49.821.97.821.846 0 1.598.542 1.865 1.345l.215.643m5.276-3.67a9.012 9.012 0 01-5.276 3.67m0 0a9 9 0 01-10.275-4.835M15.75 9c0 .896-.393 1.7-1.016 2.25" />
+                                </svg>
+                            </a>
+
+                            <a href="" target="_blank" rel="noopener noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 text-black ">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0115.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 013 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 00-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 01-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 003 15h-.75M15 10.5a3 3 0 11-6 0 3 3 0 016 0zm3 0h.008v.008H18V10.5zm-12 0h.008v.008H6V10.5z" />
+                            </svg>
+                            </a>
+
+                            <a href="" target="_blank" rel="noopener noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 text-black ">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z" />
+                            </svg>
+                            </a>
+                        </div>
+                    </span>
+
+                    <span className='w-4/5 md:w-3/6 h-[20rem] flex justify-center items-center'>
+                        <img src="/src/assets/images/hero.svg" alt="hero image" />
                     </span>
                 </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,17 +7,17 @@ function Navbar() {
 	
 	return (
 		<div className='shadow text-black h-14 flex justify-center items-center'>
-			<div className='w-full lg:max-w-3xl flex justify-between items-center px-12 text-sm'>
+			<div className='w-full lg:max-w-5xl flex justify-between items-center px-12 md:px-0 text-sm'>
 				{/* desktop */}
 				<ul className='hidden w-[18rem] md:flex justify-between items-center'>
-					<li className='font-medium text-sm hover:text-gray-200 cursor-pointer transition-all'>Home</li>
-					<li className='font-medium text-sm hover:text-gray-200 cursor-pointer transition-all'>About</li>
-					<li className='font-medium text-sm hover:text-gray-200 cursor-pointer transition-all'>Services</li>
-					<li className='font-medium text-sm hover:text-gray-200 cursor-pointer transition-all'>Crypto</li>
+					<li className='font-medium text-sm hover:text-gray-500 cursor-pointer transition-all'>Home</li>
+					<li className='font-medium text-sm hover:text-gray-500 cursor-pointer transition-all'>About</li>
+					<li className='font-medium text-sm hover:text-gray-500 cursor-pointer transition-all'>Services</li>
+					<li className='font-medium text-sm hover:text-gray-500 cursor-pointer transition-all'>Crypto</li>
 				</ul>
 				<ul className='hidden w-[12rem] md:flex justify-between items-center'>
-					<li className='font-medium text-sm hover:text-gray-200 cursor-pointer transition-all'>Log In</li>
-					<li className='font-medium text-sm bg-indigo-500 hover:bg-indigo-600 hover:text-gray-200 px-7 py-2 rounded-md cursor-pointer transition-all'>Sign Up</li>
+					<li className='font-medium text-sm hover:text-gray-500 cursor-pointer transition-all'>Log In</li>
+					<li className='font-medium text-sm bg-indigo-500 hover:bg-indigo-600 text-white hover:text-gray-300 px-7 py-2 rounded-md cursor-pointer transition-all'>Sign Up</li>
 				</ul>
 
 				{/* mobile */}
@@ -29,7 +29,7 @@ function Navbar() {
 					</li>
 				</ul>
 				{/* options panel */}
-				<div className={isNavOpen ? 'md:hidden absolute top-12 right-0 h-screen w-full' : 'hidden'}>
+				<div className={isNavOpen ? 'md:hidden bg-[#f1f1f1] absolute top-12 right-0 h-screen w-full' : 'hidden'}>
 					<ul className="h-full flex flex-col justify-center items-center gap-y-5">
 						<li className='hover:text-gray-400 text-xl font-bold hover:underline cursor-pointer uppercase'>Home</li>
 						<li className='hover:text-gray-400 text-xl font-bold hover:underline cursor-pointer uppercase'>About</li>
@@ -38,8 +38,8 @@ function Navbar() {
 					</ul>
 				</div>
 				<ul className='md:hidden w-[12rem] flex justify-between items-center'>
-					<li className='font-medium text-sm hover:text-gray-200 cursor-pointer transition-all'>Log In</li>
-					<li className='font-medium text-sm bg-indigo-500 hover:bg-indigo-600 hover:text-gray-200 px-7 py-2 rounded-md cursor-pointer transition-all'>Sign Up</li>
+					<li className='font-medium text-sm hover:text-gray-500 cursor-pointer transition-all'>Log In</li>
+					<li className='font-medium text-sm text-white bg-indigo-500 hover:bg-indigo-600 hover:text-gray-300 px-7 py-2 rounded-md cursor-pointer transition-all'>Sign Up</li>
 				</ul>
 			</div>
 		</div>
